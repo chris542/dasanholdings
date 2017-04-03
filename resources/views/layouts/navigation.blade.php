0503853f6cee9ba35fdbbe0902ba0df9cd828505
Chris <chris.sm.kang542@gmail.com>
2017-04-03 13:44:51 +1200

    updating navigation bar

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-default">
+<nav class="navbar navbar-default cart-nav">
         <div class="container">
             <ul class="nav navbar-nav">
                 <li><a href=""><i class="fa fa-shopping-cart" aria-hidden="true"></i> My Cart</a></li>
@@ -9,15 +9,28 @@
             </ul>
         </div>
 </nav>
-<nav class="navbar navbar-default">
+<nav class="navbar navbar-default search-nav">
         <div class="container">
-            <ul class="nav navbar-nav">
-                <li><a href=""><i class="fa fa-shopping-cart" aria-hidden="true"></i> My Cart</a></li>
-            </ul>
-            <ul class="nav navbar-nav navbar-right">
-              <li><a href="#"><span class="glyphicon glyphicon-user"></span> Register</a></li>
-              <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
-            </ul>
+            <div class="col-sm-2 navbar-logo">
+                <a href="/">
+                    <img src="img/logo/Logo.png" alt="DasanHoldingsLtdLogo">
+                </a>
+            </div>
+            <div class="col-sm-10 form-group navbar-search">
+                    <div class="input-group">
+                        <input type="text" class="form-control" placeholder="Search Product..">
+                        <span class="input-group-btn">
+                        <button class="btn btn-default nav-search-btn" type="button"><i class="glyphicon glyphicon-search"></i></button>
+                  </span>
+                </div>          
+            </div>
         </div>
+</nav>
+<nav class="nav-tabs">
+    <div class="container">
+        <ul class="nav nav-pills">
+          <li role="presentation" class="active"><a href="#">Home</a></li>
+        </ul>
+    </div>
 </nav>
 
