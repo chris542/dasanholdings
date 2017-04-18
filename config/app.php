90c554e52df714764fd5ac6b66ca27e3b9946dbb
Chris <chris.sm.kang542@gmail.com>
2017-04-18 14:29:01 +1200

    anam/phpcart added to the project for cart system

--- config/app.php
+++ config/app.php
@@ -177,6 +177,8 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        //Cart
+        'Anam\Phpcart\CartServiceProvider'
     ],
 
     /*
@@ -225,7 +227,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-
+        'Cart' => 'Anam\Phpcart\Facades\Cart'
     ],
 
 ];
